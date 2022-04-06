a6a53c2dd15977020a968067d2a27606a0206e48
hyeri <hyerijung0903@gmail.com>
2022-04-06 16:32:35 +0900

    Reusable components with JSX+Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+function Food({ fav }){
+  console.log(fav);
+  return (
+      <h3>I love {fav}</h3>
+  );
+}
+
 function App() {
   return (
     <div>
-      Hello~
+      <h1>Hello</h1>
+      <Food 
+        fav="kimchi" 
+        something={true}
+        papa={"hello", 1, 2, 3, 4}
+      />
+      <Food fav="chocolate" />
+      <Food fav="icecream" />
     </div>
   );
 }
