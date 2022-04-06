84b062a11aef4b50159c769cd43d8ac808c52859
hyeri <hyerijung0903@gmail.com>
2022-04-06 17:04:39 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ name }){
+function Food({ name, rating}){
   console.log(name);
   return (
     <div>
       <h3>I love {name}</h3>
+      <h4>{rating}/5.0</h4>
     </div>
      
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+}
+
 const foodILike = [
   {
-    name: "Kimchi"
+    name: "Kimchi",
+    rating: 1
   },
   {
-    name: "Ice-cream"
+    name: "Ice-cream",
+    rating: 2
   },
   {
-    name: "Chocolate"
+    name: "Chocolate",
+    rating: 3
   }
 ]
+
+function renderFood(dish) {
+  console.log(dish);
+  return <Food name={dish.name} rating={dish.rating} />
+}
 
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      {foodILike.map(dish => (
-        <Food name={dish.name} />
-      ))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
